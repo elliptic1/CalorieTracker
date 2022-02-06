776708ace066594635627970183d510d6eb27091
Todd B. Smith <elliptic1@gmail.com>
2022-02-06 16:37:36 -0500

    Move to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        // https://developer.android.com/jetpack/androidx/releases/compose-compiler
-        compose_version = '1.0.5'
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.4"
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31'
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by toddsmith on 2/5/22.
+ */
+
+object DaggerHilt {
+    const val version = "2.40"
+    const val hiltAndroid = "com.google.dagger:hilt-android:$version"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
+}
